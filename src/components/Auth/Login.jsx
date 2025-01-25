f2aa4362192f851a7ab85bc0e58de18476f9e4f9
bose <yrmewara@student.sfit.ac.in>
2025-01-25 15:29:45 +0530

    Third commit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate, Link as RouterLink } from 'react-router-dom';
 import { loginAsync } from '../../store/authSlice';
 import { initializeUserTasks } from '../../store/taskSlice';
 import {
   Box,
-  Paper,
   TextField,
   Button,
   Typography,
+  Paper,
   Container,
+  Link as MuiLink,
   Alert,
-  CircularProgress,
-  Link,
+  CircularProgress
 } from '@mui/material';
 
 const Login = () => {
@@ -25,7 +25,7 @@ const Login = () => {
     password: '',
     general: ''
   });
-  
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
@@ -34,22 +34,26 @@ const Login = () => {
     return <Navigate to="/" />;
   }
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+    // Clear error when user starts typing
+    if (errors[name]) {
+      setErrors(prev => ({
+        ...prev,
+        [name]: ''
+      }));
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if (!formData.email || !formData.password) {
-      setErrors({
-        email: !formData.email ? 'Email is required' : '',
-        password: !formData.password ? 'Password is required' : '',
-        general: ''
-      });
-      return;
-    }
-
     try {
       const resultAction = await dispatch(loginAsync(formData));
       if (loginAsync.fulfilled.match(resultAction)) {
-        // Initialize tasks for the user after successful login
         dispatch(initializeUserTasks(formData.email));
         navigate('/');
       }
@@ -57,21 +61,6 @@ const Login = () => {
       setErrors({
         ...errors,
         general: 'Login failed. Please try again.'
-      });
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-    // Clear error when user starts typing
-    if (errors[name]) {
-      setErrors({
-        ...errors,
-        [name]: ''
       });
     }
   };
@@ -86,61 +75,70 @@ const Login = () => {
           alignItems: 'center',
         }}
       >
-        <Paper elevation={3} sx={{ p: 4, width: '100%' }}>
-          <Typography component="h1" variant="h5" align="center" gutterBottom>
-            Sign In
+        <Paper
+          elevation={3}
+          sx={{
+            p: 4,
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2
+          }}
+        >
+          <Typography variant="h5" component="h1" align="center">
+            Login
           </Typography>
-          
+
           {errors.general && (
-            <Alert severity="error" sx={{ mb: 2 }}>
-              {errors.general}
-            </Alert>
+            <Alert severity="error">{errors.general}</Alert>
           )}
-          
-          <Box component="form" onSubmit={handleSubmit} noValidate>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={formData.email}
-              onChange={handleChange}
-              error={!!errors.email}
-              helperText={errors.email}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={formData.password}
-              onChange={handleChange}
-              error={!!errors.password}
-              helperText={errors.password}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={loading}
-            >
-              {loading ? <CircularProgress size={24} /> : 'Sign In'}
-            </Button>
-            <Box sx={{ textAlign: 'center' }}>
-              <Link component={RouterLink} to="/register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
+
+          <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+            <Box sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formData.email}
+                onChange={handleChange}
+                error={!!errors.email}
+                helperText={errors.email}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleChange}
+                error={!!errors.password}
+                helperText={errors.password}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={loading}
+              >
+                {loading ? <CircularProgress size={24} /> : 'Sign In'}
+              </Button>
+              <Box sx={{ textAlign: 'center' }}>
+                <MuiLink component={RouterLink} to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </MuiLink>
+              </Box>
             </Box>
-          </Box>
+          </form>
         </Paper>
       </Box>
     </Container>
